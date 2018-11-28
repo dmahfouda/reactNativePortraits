7a650e22b8d304e0ba1c757cac6a6180fb44e030
gebgebgeb <asdf>
2018-11-28 13:57:05 -0500

    mouse down listener

--- App.js
+++ App.js
@@ -3,19 +3,57 @@ import { StyleSheet, Text, View } from 'react-native';
 import Canvas from 'react-native-canvas';
 
 export default class App extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			mouseDown: false
+			, lastMousePos: null
+			, portraitHistory: {
+					mousePositionArray: []
+				}
+		}
+	}
   
   handleCanvas = (canvas) => {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
+    ctx.fillRect(0, 0, 200, 200);
   }
+
+	getAndStoreMousePos = (evt, mouseUpBool) => {
+		let newPH = this.state.portraitHistory
+		newPH.mousePositionArray.push({
+			x: evt.nativeEvent.locationX
+			, y: evt.nativeEvent.locationY
+			, mouseUp: mouseUpBool
+			, color: this.currentStrokeColor 
+		})
+		this.setState({portraitHistory: newPH})
+		return {
+			x: evt.nativeEvent.locationX
+			, y: evt.nativeEvent.locationY
+		}
+	}
+
+	mouseDownListener = (evt) => {
+		this.setState({
+			mouseDown: true
+			, lastMousePos: this.getAndStoreMousePos(evt ,false)
+		})
+	}
 
   render() {
     return (
-      <View>
+			<View 
+				onStartShouldSetResponder={()=>true}
+				onResponderStart={this.mouseDownListener}
+				//onResponderMove
+				//onResponderRelease
+			>
         <Text>Dont open up App.js to start working on your app!</Text>
         <Canvas ref={this.handleCanvas}/>
       </View>
     );
   }
 }
+
