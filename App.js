9bf9a47540fb5fff6c4ced2404e07fd2c5adfe28
dmahfouda <david.mahfouda@gmail.com>
2018-11-29 14:22:59 -0500

    maybe we messed stuff up

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Canvas from 'react-native-canvas';
+// import WebRTC from 'react-native-webrtc';
+
+// console.log(WebRTC)
+
+// var {
+//   RTCPeerConnection,
+//   RTCIceCandidate,
+//   RTCSessionDescription,
+//   RTCView,
+//   MediaStream,
+//   MediaStreamTrack,
+//   getUserMedia,
+// } = WebRTC;
 
 export default class App extends React.Component {
 	constructor(props){
@@ -15,7 +28,55 @@ export default class App extends React.Component {
 					mousePositionArray: []
 				}
 		}
+		// this.rtc()
 	}
+
+	// rtc = () => {
+	// 	// var configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+
+	// 	var configuration = {"iceServers": [{"url": "turn:localhost:9000/api",}], "peerIdentity":12}
+	// 	// peer = new Peer(id, {key:'peerjs', port:9000, host:'localhost', path: '/api', debug:3})
+	// 	var pc = new RTCPeerConnection(configuration)
+		
+	// 	let isFront = true;
+	// 	MediaStreamTrack.getSources(sourceInfos => {
+	// 	  console.log(sourceInfos);
+	// 	  let videoSourceId;
+	// 	  for (let i = 0; i < sourceInfos.length; i++) {
+	// 	    const sourceInfo = sourceInfos[i];
+	// 	    if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+	// 	      videoSourceId = sourceInfo.id;
+	// 	    }
+	// 	  }
+		  
+	// 	  getUserMedia({
+	// 	    audio: true,
+	// 	    video: {
+	// 	      mandatory: {
+	// 	        minWidth: 500, // Provide your own width, height and frame rate here
+	// 	        minHeight: 300,
+	// 	        minFrameRate: 30
+	// 	      },
+	// 	      facingMode: (isFront ? "user" : "environment"),
+	// 	      optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+	// 	    }
+	// 	  }, function (stream) {
+	// 	    console.log('dddd', stream);
+	// 	    callback(stream);
+	// 	  }, logError);
+	// 	});
+
+	// 	pc.createOffer(function(desc) {
+	// 	  pc.setLocalDescription(desc, function () {
+	// 	    // Send pc.localDescription to peer
+	// 	  }, function(e) {});
+	// 	}, function(e) {});
+
+	// 	pc.onicecandidate = function (event) {
+	// 	  // send event.candidate to peer
+	// 	};
+
+	// }
   
 	handleCanvas = (canvas) => {
 		const ctx = canvas.getContext('2d');
